3b233ea5ecb8b9b86a3bb904226a155acbeef932
MindsEye69 <rev.mindseye@gmail.com>
2026-01-25 19:05:33 +0100

    Fixed Top nav to include Process and Glossary

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -4,6 +4,8 @@ const nav = [
   { href: "/start-here", label: "Start Here" },
   { href: "/master", label: "The Master" },
   { href: "/papers", label: "Papers" },
+  { href: "/process", label: "Process" },
+  { href: "/glossary", label: "Glossary" },
   { href: "/ariadne", label: "Ariadne" },
   { href: "/about", label: "About" },
 ];
